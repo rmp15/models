494eaee11d7d40ecdb18bb9d2b460ebaa87fbcd4
Robbie Parks <r.parks15@imperial.ac.uk>
2016-05-20 17:31:47 +0100

    added inla fit

--- TMB/temporal/temporal_single_age.R
+++ TMB/temporal/temporal_single_age.R
@@ -6,33 +6,38 @@ library(TMB)
 # create a dataset from specified model
 
 # series length (t) and number of different series (n)
-t <- 100
-n <- 1
+T <- 10000
+N <- 1
 
 # coefficients
 alpha_0 <- 1
-beta_0 <- 0.00005
+beta_0 <- 0.001
 
 # generate random walk
 generate.rw1 <- function(log_sigma_rw) {
-dummy <-c(0,cumsum(rnorm(n=t-1, mean=0,sd=exp(log_sigma_rw))))
+dummy <-c(0,cumsum(rnorm(n=T-1, mean=0,sd=exp(log_sigma_rw))))
 return(dummy)
 }
 
-rw1 <- replicate(n,generate.rw1(0)) 
+rw1 <- replicate(N,generate.rw1(0)) 
 
 # generate covariate values
-x <- rep(seq(1:t),n)
+t <- rep(seq(1:T),N)
 
 # compute mu values
-real_mu <- exp(alpha_0 + beta_0 * x + rw1)
+real_mu <- exp(alpha_0 + beta_0 * t)# + rw1)
 
-y <- rpois(n=t, lambda=real_mu)
+y <- rpois(n=T*N, lambda=real_mu)
 
-dat <- data.frame(x=x,mu=y,log_mu=log(y))
+dat <- data.frame(t=t,mu=y)
 
-# fit using built-in glm
-fit <- glm(mu~x, family= poisson, data=dat)
+# fit using glm
+glm.fit <- glm(dat$mu ~ 1+t, family='poisson')
+
+# fit using INLA
+t2 <- t
+fml <- mu ~ 1 + t# + f(t2,model='rw1')
+inla.fit <- inla(fml,family='poisson',data=dat)
 
 # create matrix with data
 log_mu <- matrix(dat$log_mu,t,n)
