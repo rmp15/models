01440cc684ee7310da972551c8d9f8660921c3e0
Robbie Parks <r.parks15@imperial.ac.uk>
2016-05-20 18:16:54 +0100

    changed model parameters

--- TMB/temporal/temporal_single_age.R
+++ TMB/temporal/temporal_single_age.R
@@ -10,8 +10,8 @@ T <- 10000
 N <- 1
 
 # coefficients
-alpha_0 <- 1
-beta_0 <- 0.001
+alpha_0 <- 10
+beta_0 <- 0.1
 
 # generate random walk
 generate.rw1 <- function(log_sigma_rw) {
@@ -19,24 +19,24 @@ dummy <-c(0,cumsum(rnorm(n=T-1, mean=0,sd=exp(log_sigma_rw))))
 return(dummy)
 }
 
-rw1 <- replicate(N,generate.rw1(0)) 
+rw1 <- replicate(N,generate.rw1(0.00001)) 
 
 # generate covariate values
 t <- rep(seq(1:T),N)
 
 # compute mu values
-real_mu <- exp(alpha_0 + beta_0 * t)# + rw1)
+real_mu <- exp(alpha_0 + beta_0 * t + rw1)
 
 y <- rpois(n=T*N, lambda=real_mu)
 
 dat <- data.frame(t=t,mu=y)
 
 # fit using glm
-glm.fit <- glm(dat$mu ~ 1+t, family='poisson')
+# glm.fit <- glm(dat$mu ~ 1+t, family='poisson')
 
 # fit using INLA
 t2 <- t
-fml <- mu ~ 1 + t# + f(t2,model='rw1')
+fml <- mu ~ 1 + t + f(t2,model='rw1')
 inla.fit <- inla(fml,family='poisson',data=dat)
 
 # create matrix with data
