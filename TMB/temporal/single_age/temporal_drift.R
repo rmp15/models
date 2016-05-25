1153b709d4c55e1ed0ca4ee856f0e8d8294eba54
rmiparks <r.parks15@imperial.ac.uk>
2016-05-25 13:39:25 +0100

    Create temporal_drift.R

---
+++ TMB/temporal/single_age/temporal_drift.R
@@ -0,0 +1,67 @@
+library(TMB)
+
+# series length (t) and number of different series (n)
+T <- 100
+N <- 1
+
+# parameters
+alpha_0 <- 10
+beta_0 <- -0.01
+prec_rw1 <- 100
+prec_e <- 300
+
+# generate random walk
+generate.rw1 <- function(prec) {
+#set.seed(123)
+dummy <-c(0,cumsum(rnorm(n=T-1, mean=0,sd=1/sqrt((prec)))))
+return(dummy)
+}
+
+# generate random walk and overdispersion
+rw1 <- replicate(N,generate.rw1(prec_rw1)) 
+od  <- rnorm(T, mean = 0, sd = sqrt(1 / prec_e))
+
+# generate covariate values
+t <- rep(seq(1:T),N)
+
+# compute mu values
+real_lambda <- exp(alpha_0 + beta_0 * t  + rw1 + od)
+
+counts <- rpois(n=T*N, lambda=real_lambda)
+
+dat <- data.frame(t=t,counts=counts)
+
+# create matrix with data
+log_counts <- matrix(log(dat$counts),N,T)
+
+# compile cpp file
+compile('model.cpp')
+dyn.load(dynlib('model'))
+
+# prepare list of parameters for TMB
+data <- list(log_counts = log_counts)
+parameters <- list(beta_0=1.,log_tau_rw=4.,log_tau_epsilon=1.,log_counts_pred=matrix(1.,N,T), pi=matrix(0.,N,T) )
+
+# run TMB model on simulated data
+obj <- MakeADFun(data, parameters, random = "pi", DLL='model')
+obj$hessian <- FALSE
+opt <- do.call("optim", obj)
+sd <- sdreport(obj)
+
+# extract fixed effects
+fixed <- summary(sd, 'fixed')
+
+# compare results with INLA
+library(INLA)
+t3 <- t2 <- t
+fml <- counts ~ 1 + t + f(t2,model='rw1') + f(t3, model = "iid")
+inla.fit <- inla(fml,family='poisson',data=dat, control.predictor = list(link = 1))
+
+# plot results compared with original data
+library(ggplot2)
+plot.inla <- inla.fit$summary.fitted.values
+plot.inla$id <- seq(1:nrow(plot.inla))
+p <-  ggplot() +
+      geom_line(data=dat,colour='blue',aes(x=t,y=counts)) + 
+      geom_line(data=plot.inla,colour='red',aes(x=id, y=mean)) + 
+      geom_ribbon(data=plot.inla,alpha=0.5,aes(x=id,ymax=(`0.975quant`),ymin=(`0.025quant`)))
