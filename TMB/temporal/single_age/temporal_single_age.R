7385a34fc98060adb87b2d0004319a48c7d670b2
rmiparks <r.parks15@imperial.ac.uk>
2016-05-25 09:47:24 +0100

    Update temporal_single_age.R

--- TMB/temporal/single_age/temporal_single_age.R
+++ TMB/temporal/single_age/temporal_single_age.R
@@ -1,6 +1,3 @@
-rm(list=ls())
-
-library(INLA)
 library(TMB)
 
 # create a dataset from specified model
@@ -12,7 +9,8 @@ N <- 1
 # parameters
 alpha_0 <- 10
 beta_0 <- -0.01
-prec_rw1 <- 150000
+prec_rw1 <- 100
+prec_e <- 300
 
 # generate random walk
 generate.rw1 <- function(prec) {
@@ -27,33 +25,25 @@ rw1 <- replicate(N,generate.rw1(prec_rw1))
 t <- rep(seq(1:T),N)
 
 # compute mu values
-real_lambda <- exp(alpha_0 + beta_0 * t  + rw1)
+real_lambda <- exp(alpha_0 + beta_0 * t  + rw1 + rnorm(T, mean = 0, sd = sqrt(1 / prec_e)))
 
 counts <- rpois(n=T*N, lambda=real_lambda)
 
 dat <- data.frame(t=t,counts=counts)
 
-# fit using glm
-# glm.fit <- glm(dat$counts ~ 1+t, family='poisson'(link = "log"))
-
-# fit using INLA
-t2 <- t
-fml <- counts ~ 1 + t + f(t2,model='rw1')
-inla.fit <- inla(fml,family='poisson',data=dat)
-
 # create matrix with data
 log_counts <- matrix(log(dat$counts),N,T)
 
 # compile cpp file
-compile('temporal_single_age.cpp')
-dyn.load(dynlib('temporal_single_age'))
+compile('model.cpp')
+dyn.load(dynlib('model'))
 
 # prepare list of parameters for TMB
 data <- list(log_counts = log_counts)
-parameters <- list(alpha_0=1,beta_0=1,log_tau_rw=1,log_tau_epsilon=1,log_counts_pred=matrix(1,N,T))
+parameters <- list(alpha_0=0., beta_0=1.,log_tau_rw=4.,log_tau_epsilon=1.,log_counts_pred=matrix(1.,N,T), pi=matrix(0.,N,T) )
 
 # run TMB model on simulated data
-obj <- MakeADFun(data, parameters, DLL='temporal_single_age')
+obj <- MakeADFun(data, parameters, random = "pi", DLL='model')
 obj$hessian <- FALSE
 opt <- do.call("optim", obj)
 sd <- sdreport(obj)
@@ -61,3 +51,8 @@ sd <- sdreport(obj)
 # extract fixed effects
 fixed <- summary(sd, 'fixed')
 
+# compare results with INLA
+library(INLA)
+t2 <- t
+fml <- counts ~ 1 + t + f(t2,model='rw1') + f(t3, model = "iid")
+inla.fit <- inla(fml,family='poisson',data=dat)
