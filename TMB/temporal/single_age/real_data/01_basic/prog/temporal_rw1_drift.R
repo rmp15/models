b9c72a60f9d3c50659b4d53ac26ff52543b9556e
Robbie Parks <rmiparks@dyn1245-156.vpn.ic.ac.uk>
2016-06-07 22:18:05 +0100

    fixed output to pdf

--- TMB/temporal/single_age/real_data/01_basic/prog/temporal_rw1_drift.R
+++ TMB/temporal/single_age/real_data/01_basic/prog/temporal_rw1_drift.R
@@ -46,8 +46,9 @@ extract.variable <- function(sd, var, type) {
 
 # extract predicted values from TMB
 log_mu <- extract.variable(sd, 'log_mu', 'random')
-#rw1.pred <- extract.variable(sd,'pi', 'random')
-#rw1.pred$estimate <- rw1.pred$estimate - alpha_0 # accounts for intercept included in rw1
+rw1.pred <- extract.variable(sd,'pi', 'random')
+alpha_0 <- summary(sd)[grep("alpha_0", rownames(summary(sd))), 1]
+rw1.pred$estimate <- rw1.pred$estimate - alpha_0 # accounts for intercept included in rw1
 
 # obtain joint precision matrix
 joint.prec <- sd$jointPrecision
@@ -70,10 +71,10 @@ plot.inla <- inla.fit$summary.fitted.values
 plot.inla$id <- seq(1:nrow(plot.inla))
 
 tmbres <- summary(sd)[grep("^mu", rownames(summary(sd))), 1]
+
 inlares <-  inla.fit$summary.fitted.values$`0.5quant`
 plot(tmbres, inlares)
-
-
+abline(a=1,b=1)
 
 # plot fitted values from INLA and TMB compared with original data
 library(ggplot2)
@@ -84,25 +85,25 @@ p1 <-   ggplot() +
         geom_line(data=log_mu,colour='green',aes(x=id,y=exp(estimate))) +
         geom_ribbon(data=log_mu,alpha=0.2,fill='green',
 			aes(x=id,ymax=exp(estimate+1.96*std.error),ymin=exp(estimate-1.96*std.error))) +
-        ggtitle('Fitted values, red=INLA, green=TMB') +
+        ggtitle('Fitted values, blue=data, red=INLA, green=TMB') +
         theme_bw()
 
-# plot random walks from INLA and TMB compared with original data
-rw1.df <- as.data.frame(rw1)
-rw1.df$id <- seq(1:nrow(rw1.df))
-names(rw1.df) <- c('values','id')
-
+# plot random walks from INLA and TMB
 plot.inla.rw1 <- inla.fit$summary.random$t2
 plot.inla.rw1$id <- seq(1:nrow(plot.inla.rw1))
 
 p2 <-   ggplot() +
-        geom_line(data=rw1.df,aes(x=id,y=values)) +
         geom_line(data=plot.inla.rw1,colour='red',aes(x=id,y=mean)) +
         geom_ribbon(data=plot.inla.rw1,alpha=0.2,fill='red',aes(x=id,ymax=(`0.975quant`),ymin=(`0.025quant`))) +
         geom_line(data=rw1.pred,colour='green',aes(x=id,y=estimate)) +
-        geom_ribbon(data=rw1.pred,fill='green',alpha=0.2,aes(x=id,ymax=estimate+1.6449*std.error,ymin=estimate-1.6449*std.error)) +
+        geom_ribbon(data=rw1.pred,fill='green',alpha=0.2,aes(x=id,ymax=estimate+1.96*std.error,ymin=estimate-1.96*std.error)) +
         ggtitle('Random Walk: fitted values, red=INLA, green=TMB') +
         theme_bw()
+
+pdf('../output/results.pdf',paper='a4r',height=0,width=0)
+p1
+p2
+dev.off()
 
 
 
