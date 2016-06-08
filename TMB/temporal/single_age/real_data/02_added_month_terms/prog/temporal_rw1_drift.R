0b09c1e93b4e0fee7b20c3b1830509d627ee5a0e
Robbie Parks <r.parks15@imperial.ac.uk>
2016-06-08 18:04:52 +0100

    fixed alpha_m term but still problems with NaNs

--- TMB/temporal/single_age/real_data/02_added_month_terms/prog/temporal_rw1_drift.R
+++ TMB/temporal/single_age/real_data/02_added_month_terms/prog/temporal_rw1_drift.R
@@ -47,8 +47,10 @@ extract.variable <- function(sd, var, type) {
 
 # extract predicted values from TMB
 log_mu <- extract.variable(sd, 'log_mu', 'random')
-#rw1.pred <- extract.variable(sd,'pi', 'random')
-#rw1.pred$estimate <- rw1.pred$estimate - alpha_0 # accounts for intercept included in rw1
+rw1.pred <- extract.variable(sd,'pi', 'random')
+alpha_0 <- extract.variable(sd,'alpha_0','fixed')
+alpha_0 <- summary(sd)[grep("alpha_0", rownames(summary(sd))), 1]
+rw1.pred$estimate <- rw1.pred$estimate - alpha_0 # accounts for intercept included in rw1
 
 # obtain joint precision matrix
 joint.prec <- sd$jointPrecision
@@ -88,15 +90,10 @@ p1 <-   ggplot() +
         theme_bw()
 
 # plot random walks from INLA and TMB compared with original data
-rw1.df <- as.data.frame(rw1)
-rw1.df$id <- seq(1:nrow(rw1.df))
-names(rw1.df) <- c('values','id')
-
 plot.inla.rw1 <- inla.fit$summary.random$t2
 plot.inla.rw1$id <- seq(1:nrow(plot.inla.rw1))
 
 p2 <-   ggplot() +
-        geom_line(data=rw1.df,aes(x=id,y=values)) +
         geom_line(data=plot.inla.rw1,colour='red',aes(x=id,y=mean)) +
         geom_ribbon(data=plot.inla.rw1,alpha=0.2,fill='red',aes(x=id,ymax=(`0.975quant`),ymin=(`0.025quant`))) +
         geom_line(data=rw1.pred,colour='green',aes(x=id,y=estimate)) +
@@ -104,5 +101,8 @@ p2 <-   ggplot() +
         ggtitle('Random Walk: fitted values, red=INLA, green=TMB') +
         theme_bw()
 
-
+pdf('../output/results.pdf',paper='a4r',height=0,width=0)
+p1
+p2
+dev.off()
 
