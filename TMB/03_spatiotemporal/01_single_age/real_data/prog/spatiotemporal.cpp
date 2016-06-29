32e2d9f922317be4ae0d846ec0b0415ac2cf7193
Robbie Parks <r.parks15@imperial.ac.uk>
2016-06-29 17:08:12 +0100

    adapted spatiotemporal

--- TMB/03_spatiotemporal/01_single_age/real_data/prog/spatiotemporal.cpp
+++ TMB/03_spatiotemporal/01_single_age/real_data/prog/spatiotemporal.cpp
@@ -4,43 +4,53 @@
 //  using namespace density;
 
 template<class Type>
-Type objective_function<Type>::operator() () {
-	DATA_VECTOR(E);
-	DATA_VECTOR(deaths);
-	DATA_SPARSE_MATRIX(P); // precision matrix
+Type objective_function<Type>::operator() () 
+{
+	
+// DATA
+DATA_MATRIX(deaths);		// matrix of death counts for single age group in multiple states
+DATA_MATRIX(E);		    	// matrix of population for single age group in multiple states
+DATA_SPARSE_MATRIX(P); 		// precision matrix for adjacency
+size_t T = deaths.cols();   	// number of time points
+size_t N = deaths.rows();   	// number of states
 
-	PARAMETER(alpha);
-	PARAMETER(log_sigma2_V);
-	PARAMETER_VECTOR(V);
+// PARAMETERS
+PARAMETER(alpha);
+PARAMETER(log_sigma2_V);
+PARAMETER_MATRIX(V);
 
-	PARAMETER(log_sigma2_U);
-	PARAMETER_VECTOR(W);
+PARAMETER(log_sigma2_U);
+PARAMETER_MATRIX(W);
 
-	int N = E.size();
+// DEFINE DEATHS COUNTS DEATH RATE
+matrix<Type> log_deaths_pred(N,T);
+matrix<Type> deaths_pred = exp(log_deaths_pred);
+matrix<Type> mu(N,T);
 
-	vector<Type> log_deaths_pred(N);
-	vector<Type> mu(N);
+// INITIALISE NEGATIVE LOG-LIKELIHOOD
+Type nll = 0;
 
-	Type nll = 0;
+// PRECISIONS
+Type prec_V = 1 / exp(log_sigma2_V);
+Type prec_U = 1 / exp(log_sigma2_U);
 
-	Type tau_V = 1 / exp(log_sigma2_V);
-	Type tau_U = 1 / exp(log_sigma2_U);
+// ASSIGN HYPERPRIORS 
+nll -= dnorm(alpha, Type(0), Type(10), 1);
+nll -= dgamma(prec_V, Type(0.5), Type(2000), 1);
+nll -= dgamma(prec_U, Type(0.5), Type(2000), 1);
+nll -= dnorm(V, Type(0), exp(0.5 * log_sigma2_V), 1).sum();
 
-	nll -= dnorm(alpha, Type(0), Type(10), 1);
-	nll -= dgamma(tau_V, Type(0.5), Type(2000), 1);
-	nll -= dgamma(tau_U, Type(0.5), Type(2000), 1);
-	nll -= dnorm(V, Type(0), exp(0.5 * log_sigma2_V), 1).sum();
-
-	vector<Type> tmp = P * W;
-	nll -= -0.5 * (W * tmp).sum();
-	vector<Type> U = W * exp(0.5 * log_sigma2_U);
+// I DON'T UNDERSTAND THIS BIT ONWARDS...
+matrix<Type> tmp = P * W;
+nll -= -0.5 * (W * tmp).sum();
+vector<Type> U = W * exp(0.5 * log_sigma2_U);
 
 	nll -= dnorm(U.sum(), Type(0), Type(0.00001), 1);
 	for (size_t i = 0; i < N; i++) log_deaths_pred(i) = log(E(i)) + alpha + V(i) + U(i);
 	for (size_t i = 0; i < N; i++) nll -= dpois(deaths(i), exp(log_deaths_pred(i)), 1);
 	for (size_t i = 0; i < N; i++) mu(i) = exp(alpha + V(i) + U(i));
 
-	vector<Type> deaths_pred = exp(log_deaths_pred);
+
 
 	ADREPORT(U);
 	ADREPORT(deaths_pred);
